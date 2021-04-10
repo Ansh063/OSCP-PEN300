26e6a69036a4f4e887ad1f988790f3a765516e4a
Ansh Goyal <goyalansh063@gmail.com>
2021-04-10 10:22:34 +0530

    PEN200

---
+++ PEN 200/ACTIVE DIRECTORY ATTACKS/EnumeratingAD.ps1
@@ -0,0 +1,34 @@
+﻿$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain() $PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+$SearchString
+
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString) 
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="samAccountType=805306368"
+# or we can use a specific user name 
+
+# $Searcher.filter="name=Jeff_Admin"
+
+$Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+	Foreach($prop in $obj.Properties)
+	{
+		$prop
+	}
+	
+	Write-Host "------------------------" 
+}
